72e75dac21356d3e4648c8e5e7bbdd99c9895e2b
Harry <harryspitsillides@hotmail.com>
2017-11-28 07:55:09 +0000

    Implementing FSM

---
+++ src/fsm/seek.state.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const state_1 = require("./state");
+class SeekState extends state_1.State {
+    enter() {
+    }
+    leave() {
+    }
+    update() {
+    }
+}
+exports.SeekState = SeekState;
+//# sourceMappingURL=seek.state.js.map
---
+++ src/fsm/seek.state.ts
@@ -0,0 +1,14 @@
+import {State} from './state';
+
+export class SeekState extends State{
+
+  enter(): void {
+  }
+
+  leave(): void {
+  }
+
+  update(): void {
+  }
+
+}
